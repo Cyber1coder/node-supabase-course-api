d1880fe5e7f75c3771676da4fb66c73af4cc4ef4
Revatee Ghatage <ghatagerevati3@gmail.com>
2026-03-08 17:00:08 +0530

    commit

--- routes/courses.js
+++ routes/courses.js
@@ -1,39 +1,36 @@
 const express = require("express");
 const router = express.Router();
-const supabase = require("../middleware/validateEnrollment")
+const supabase = require("../supaBaseClient");
 
-router.get("/courses",async(req,res)=>{
-    const{data,error} = await supabase
+router.get("/courses", async (req, res) => {
+
+  const { data, error } = await supabase
     .from("courses")
-    .select("*")
+    .select("*");
 
-    if(error){
-        return
-        res.status(500).json({error:error.message})
-    }
-    res.json({
-        message:"Student enrolled successfully",
-        
-    })
-    
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json(data);
 });
 
-router.get("/courses/:id/enrollments",
-    async (req,res)=>{
-        const courseId = req.params.id;
-        
-        const {data,error} = await supabase
-            .from("enrollments")
-            .select("*")
-            .eq("course_id",courseid);
 
-            if (error){
-                return res.status(500).json({error: error.message})
-            }
-            res.json.(data);
-            
-        
-    }
-);
+router.get("/courses/:id/enrollments", async (req, res) => {
+
+  const courseId = req.params.id;
+
+  const { data, error } = await supabase
+    .from("enrollments")
+    .select("*")
+    .eq("course_id", courseId);
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
+  }
+
+  res.json(data);
+
+});
 
 module.exports = router;
